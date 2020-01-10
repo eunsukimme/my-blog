59dd030247c0a8e8be543c4ce048ff78c79478ea
eunsukimme <eunsu.dev@gmail.com>
2020-01-10 14:36:46 +0900

    Delete 'Contact Me' Menu because of preparing

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,10 +25,6 @@ module.exports = {
         label: 'About me',
         path: '/about/',
       },
-      {
-        label: 'Contact me',
-        path: '/contact/',
-      },
     ],
     author: {
       name: 'Eunsu Kim',
